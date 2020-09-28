18718b98f5d960f69ccd2edbe6f51407e3935fae
Miguel Lemos <mimiguelsl65@gmail.com>
2020-09-28 22:37:48 +0100

    update on frontend

--- src/main/java/com/luxclusifmiguel/challenge/frontend/challenge/src/pages/form.jsx
+++ src/main/java/com/luxclusifmiguel/challenge/frontend/challenge/src/pages/form.jsx
@@ -36,43 +36,105 @@ class SellingForm extends Component {
 
     render() {
 
-        const {firstName, lastName, email, country, phone, } = this.state; 
+        const {firstName, lastName, email, country, phone, brand, condition, size} = this.state; 
 
         return (
             <div>
                 <h1 align="center">FARFETCH SECOND LIFE.</h1>
+                <Link to="/">
+                    <Button variant="light" align="left" className="mr-2">Home page</Button>
+                </Link>
                 <Card className="App">
                     <Form onSubmit={this.makePostRequests} id="form">
                         <Card.Body>
                             <Form.Row>
-                                <Form.Group as={Col} controlId="formGridFirst">
+                                <Form.Group as={Col} controlId="formGridFirstName">
                                     <Form.Label>First Name</Form.Label>
                                     <Form.Control required
-                                        type="text" name="first name"
+                                        type="text" name="firstName"
                                         value={firstName}
                                         onChange={this.handleChange}
                                         className="form"
                                         placeholder="Enter first name" />
                                 </Form.Group>
-                                <Form.Group as={Col} controlId="formGridLast">
+                                <Form.Group as={Col} controlId="formGridLastName">
                                     <Form.Label>Last Name</Form.Label>
                                     <Form.Control required
-                                        type="text" name="first name"
-                                        value={this.state.firstName}
+                                        type="text" name="lastName"
+                                        value={lastName}
                                         onChange={this.handleChange}
                                         className="form"
-                                        placeholder="Enter first name" />
+                                        placeholder="Enter last name" />
+                                </Form.Group>
+                                </Form.Row>
+                                <Form.Row>
+                                <Form.Group as={Col} controlId="formGridEmail">
+                                    <Form.Label>Email</Form.Label>
+                                    <Form.Control required
+                                        type="email" name="email"
+                                        value={email}
+                                        onChange={this.handleChange}
+                                        className="form"
+                                        placeholder="something@example.com" />
+                                </Form.Group>
+                                <Form.Group as={Col} controlId="formGridCountry">
+                                    <Form.Label>Country</Form.Label>
+                                    <Form.Control required
+                                        type="text" name="country"
+                                        value={country}
+                                        onChange={this.handleChange}
+                                        className="form"
+                                        placeholder="Enter your country" />
+                                </Form.Group>
+                                </Form.Row>
+                                <Form.Row>
+                                <Form.Group as={Col} controlId="formGridPhone">
+                                    <Form.Label>Phone Number</Form.Label>
+                                    <Form.Control required
+                                        type="text" name="phone"
+                                        value={phone}
+                                        onChange={this.handleChange}
+                                        className="form"
+                                        placeholder="Enter phone number" />
+                                </Form.Group>
+                                <Form.Group as={Col} controlId="formGridBrand">
+                                    <Form.Label>Brand</Form.Label>
+                                    <Form.Control required
+                                        type="text" name="brand"
+                                        value={brand}
+                                        onChange={this.handleChange}
+                                        className="form"
+                                        placeholder="Enter your bag's brand" />
+                                </Form.Group>
+                                </Form.Row>
+                                <Form.Row>
+                                <Form.Group as={Col} controlId="formGridCondition">
+                                    <Form.Label>Condition</Form.Label>
+                                    <Form.Control required
+                                        type="text" name="condition"
+                                        value={condition}
+                                        onChange={this.handleChange}
+                                        className="form"
+                                        placeholder="Very good, good, excelent" />
+                                </Form.Group>
+                                <Form.Group as={Col} controlId="formGridSize">
+                                    <Form.Label>Size</Form.Label>
+                                    <Form.Control required
+                                        type="text" name="size"
+                                        value={size}
+                                        onChange={this.handleChange}
+                                        className="form"
+                                        placeholder="Small, Medium, Large" />
                                 </Form.Group>
                             </Form.Row>
                         </Card.Body>
                     </Form>
                 </Card>
+                <div className="App">
                 <Link to="/">
                     <button type="button" class="btn btn-dark">Get your offer</button>
                 </Link>
-                <Link to="/">
-                    <Button variant="light" className="mr-2">Home page</Button>
-                </Link>
+                </div>
             </div>
         );
 
